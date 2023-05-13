24574728e1c0af51277acc30eefb68a934643371
okomarda <okomarda@yandex.ru>
2023-05-13 12:07:03 +0300

    Внесены изменения в файлы item.py и test_item.py

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+
+file = 'items.csv'
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -5,13 +9,38 @@ class Item:
     pay_rate = 1.0
     all = []
 
-
     def __init__(self, name: str, price: float, quantity: int):
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        all.append(self)
 
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open ("C:\lessons\homework_13.1._electronshop\src\items.csv", newline='') as csvfile :
+            reader = csv.DictReader (csvfile)
+            for row in reader:
+                row_list = list(row.values())
+                item = Item(row_list[0], row_list[1], row_list[2])
+                all.append(item)
+            return all
+
+    @staticmethod
+    def string_to_number(number):
+        if number == str(float(number)):
+            return int(float(number))
+        elif number == str (int (number)):
+            return int (number)
+
+    @property
+    def name(self):
+        return self.__name
+    @name.setter
+    def name(self, name):
+        if len(name) <= 10:
+            self.__name = name
+        elif len(name) > 10:
+            print("Количество символов в наименовании товаров превышает 10")
 
     def calculate_total_price(self) -> float:
         return self.price*self.quantity
@@ -23,12 +52,18 @@ class Item:
 
 all = Item.all
 
-#item_1 = Item("Помидоры", 50.5, 100)
-#item_2 = Item("Огурцы", 30.5, 200)
+#item = Item("Помидоры", 50.5, 100)
+#print(item.name)
+#item.name = "телефон"
+#print(item.name)
+#item.name = "супертелефон"
+#print(item.name)
 
-#print(item_1.name)
-#print(item_1.calculate_total_price())
 
-#Item.pay_rate = 0.8
-#print(item_1.apply_discount())
+#print(Item.instantiate_from_csv())
 #print(Item.all)
+#print(Item.all[0])
+#item1 = Item.all[0]
+#print(item1.name)
+
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,14 +1,22 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
 
-item1 = Item ("Смартфон", 10000, 20)
-item2 = Item ("Ноутбук", 20000, 5)
+item1 = Item("Смартфон", 10000, 20)
+item2 = Item("Ноутбук", 20000, 5)
 item1.pay_rate = 0.8
 item2.pay_rate = 0.9
+item2.name = "Суперсмартфон"
+
 
 def test_item():
     assert Item.calculate_total_price(item1) == 200000
-    assert Item.calculate_total_price (item2) == 100000
+    assert Item.calculate_total_price(item2) == 100000
     assert Item.apply_discount(item1) == 8000
-    assert Item.apply_discount (item2) == 18000
+    assert Item.apply_discount(item2) == 18000
+    assert item2.name == "Ноутбук"
+    assert item1.name == "Смартфон"
+    assert len(Item.instantiate_from_csv()) == 5
+    assert Item.string_to_number('5.45') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('77') == 77
 
