bf8706bb65d604cb7dcaf272fda5fb7fa7c3e25e
okomarda <okomarda@yandex.ru>
2023-06-12 10:46:57 +0300

    Внесены изменения в файл keyboard.py

--- src/keyboard.py
+++ src/keyboard.py
@@ -4,8 +4,16 @@ class Mixing_lang():
     lang_count = 0
 
     def __init__(self):
-        pass
-
+        self._language = "EN"
+    @property
+    def language(self):
+        return self._language
+    @language.setter
+    def language(self, value):
+        if value == "RU" or value == "EN":
+            return self._language
+        else:
+            raise AttributeError("property 'language' of 'KeyBoard' object has no setter")
     def change_lang(self):
         self.lang_count += 1
         if self.lang_count % 2 == 0:
@@ -15,35 +23,22 @@ class Mixing_lang():
             self._language = "RU"
             return self
 
-
 class KeyBoard(Item, Mixing_lang):
 
-    def __init__(self, name, price, quantity, language="EN"):
+    def __init__(self, name, price, quantity):
         super().__init__(name, price, quantity)
-        self._language = language
-
-    @property
-    def language(self):
-        return self._language
-
-    @language.setter
-    def language(self, value):
-        if value == "RU" or value == "EN":
-            return self._language
-        else:
-            raise AttributeError("property 'language' of 'KeyBoard' object has no setter")
+        Mixing_lang.__init__ (self)
 
 
 
-
-# kb = KeyBoard('Dark Project KD87A', 9600, 5)
-# print(kb.language)
-# # kb.change_lang()
-# # print(kb.language)
-# # kb.change_lang()
-# # print(kb.language)
-# #kb.language = "CH"
-# kb.change_lang().change_lang().change_lang().change_lang()
-# print(kb.language)
+kb = KeyBoard('Dark Project KD87A', 9600, 5)
+print(kb.language)
+kb.change_lang()
+print(kb.language)
+kb.change_lang()
+print(kb.language)
+#kb.language = "CH"
+kb.change_lang().change_lang().change_lang().change_lang().change_lang()
+print(kb.language)
 
 
